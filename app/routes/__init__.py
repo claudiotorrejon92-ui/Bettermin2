1fba2dd860750c44ff5fa53d84047b04b02b30a1
claudiotorrejon92-ui <claudio.torrejon92@gmail.com>
2025-09-07 15:59:59 -0700

    Include lab router in routes index

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter
 from .lotes import router as lotes_router
 from .muestras import router as muestras_router
 from .ml import router as ml_router
+from .lab import router as lab_router
 
 api_router = APIRouter()
 api_router.include_router(lotes_router, prefix="/lotes", tags=["lotes"])
+
 api_router.include_router(muestras_router, prefix="/muestras", tags=["muestras"])
 api_router.include_router(ml_router, prefix="/ml", tags=["ml"])
+api_router.include_router(lab_router, prefix="/lab", tags=["lab"])
