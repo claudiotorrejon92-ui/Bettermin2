2abec265ef9eea9b9074aba37fc5a81c6f3f6f70
claudiotorrejon92-ui <claudio.torrejon92@gmail.com>
2025-09-07 15:38:48 -0700

    Include ML router in routes index

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .lotes import router as lotes_router
 from .muestras import router as muestras_router
+from .ml import router as ml_router
 
 api_router = APIRouter()
 api_router.include_router(lotes_router, prefix="/lotes", tags=["lotes"])
 api_router.include_router(muestras_router, prefix="/muestras", tags=["muestras"])
+api_router.include_router(ml_router, prefix="/ml", tags=["ml"])
