61e0da1f127437d9a0a252b88e954dd37b093cc1
claudiotorrejon92-ui <claudio.torrejon92@gmail.com>
2025-09-06 19:10:22 -0700

    Create __init__.py

---
+++ app/routes/__init__.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from .lotes import router as lotes_router
+from .muestras import router as muestras_router
+
+api_router = APIRouter()
+api_router.include_router(lotes_router, prefix="/lotes", tags=["lotes"])
+api_router.include_router(muestras_router, prefix="/muestras", tags=["muestras"])
